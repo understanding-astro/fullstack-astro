272d23526c213cf64d90bbcc7fc178c03ff9b603
ohansemmanuel <ohansemmanuel@outlook.com>
2023-05-29 14:50:58 +0200

    ✨ feat: change client init config

--- src/scripts/firebase/init.ts
+++ src/scripts/firebase/init.ts
@@ -7,14 +7,15 @@ import { getAuth, connectAuthEmulator } from "firebase/auth";
 /**
  * The following represent the firebase client configuration for the application.
  * These are safe to be exposed on the client.
+ * ⚠️ TODO: change these to match your project configuration.
  */
 const firebaseConfig = {
-  projectId: "be-audible",
-  messagingSenderId: "376052341346",
-  authDomain: "be-audible.firebaseapp.com",
-  storageBucket: "be-audible.appspot.com",
-  apiKey: "AIzaSyBphMfuOCAshU7BkkttdvKeZFtnGdfRJmg",
-  appId: "1:376052341346:web:2a082c97d822e10667cf3b",
+  projectId: "XXXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXX",
+  apiKey: "XXXXXXXXXXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXX",
 };
 
 // Initialize Firebase apps
