04d6c4005bb438df74620ca367e73cbd4be3b862
ohansemmanuel <ohansemmanuel@outlook.com>
2023-05-29 09:43:46 +0200

    ✨ feat: cleans up components

--- src/components/AudioRecorder.tsx
+++ src/components/AudioRecorder.tsx
@@ -1,6 +1,9 @@
 import { useStore } from "@nanostores/react";
 import { VoiceRecorder } from "react-voice-recorder-player";
 import { $audioRecording, uploadRecording } from "@stores/audioRecording";
+import UploadingRecording from "@components/UploadingRecording.astro";
+import RecordingPublishFailed from "@components/RecordingPublishFailed.astro";
+import RecordingPublishSuccess from "@components/RecordingPublishSuccess.astro";
 
 type Props = {
   cta?: string;
@@ -22,26 +25,20 @@ export const Recorder = (props: Props) => {
       );
 
     case "uploading":
-      return (
-        <div className="flex items-center justify-center w-56 h-56 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-          <div className="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">
-            Uploading ...
-          </div>
-        </div>
-      );
+      return <UploadingRecording>Uploading ...</UploadingRecording>;
 
     case "failed":
       return (
-        <div className="bg-red-400 rounded-md py-6 px-3 text-slate-100 motion-safe:animate-bounce">
+        <RecordingPublishFailed>
           An error occurred uploading your recording
-        </div>
+        </RecordingPublishFailed>
       );
 
     case "completed":
       return (
-        <div className="bg-green-400 rounded-md py-6 px-3 text-slate-100 motion-safe:animate-bounce">
+        <RecordingPublishSuccess>
           Successfully published your recording!
-        </div>
+        </RecordingPublishSuccess>
       );
 
     default:
